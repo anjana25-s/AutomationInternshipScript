7f7627b8380234c5256b51b90bcdc43aba4ebd90
Tango <tango@promilo.com>
2025-12-24 15:19:16 +0530

    updated

--- src/main/java/com/promilo/automation/mentorship/mentor/CampaignDetails.java
+++ src/main/java/com/promilo/automation/mentorship/mentor/CampaignDetails.java
@@ -4,8 +4,7 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 
 public class CampaignDetails {
-	
-	
+
 	private final Page page;
 	private final Locator campaignName;
 	private final Locator skills;
@@ -14,31 +13,46 @@ public class CampaignDetails {
 	private final Locator saveButton;
 	private final Locator clickheretoAddskills;
 	private final Locator skillName;
+
 	public CampaignDetails(Page page) {
-		
-		this.page= page;
-		this.campaignName= page.locator("//input[@id='campaign_name']");
-		this.skills= page.locator("//input[@placeholder='Search or add keyword...']");
-		this.startDate= page.locator("#start_date");
-<<<<<<< HEAD
-		this.endDate= page.locator("//input[@id='end_date']");
-=======
-		this.endDate= page.locator("(//input[@id='end_date'])[1]");
->>>>>>> refs/remotes/origin/mentorship-Automation-on-Mentorship-Automation
-		this.saveButton= page.locator("//button[text()='Save and Next']");
-		this.clickheretoAddskills= page.locator("//div[text()='+ Click here to add']");
-		this.skillName= page.locator("//div[text()='Automation Testing']");
-		
-		
+
+		this.page = page;
+		this.campaignName = page.locator("//input[@id='campaign_name']");
+		this.skills = page.locator("//input[@placeholder='Search or add keyword...']");
+		this.startDate = page.locator("#start_date");
+		this.endDate = page.locator("(//input[@id='end_date'])[1]");
+		this.saveButton = page.locator("//button[text()='Save and Next']");
+		this.clickheretoAddskills = page.locator("//div[text()='+ Click here to add']");
+		this.skillName = page.locator("//div[text()='Automation Testing']");
+
 	}
-	
-	public Locator campaignName() {return campaignName;}
-	public Locator skills() {return skills;}
-	public Locator startDate() {return startDate;}
-	public Locator endDate() {return endDate;}
-	public Locator saveButton() {return saveButton;}
-	public Locator clickheretoAddskills() {return clickheretoAddskills;}
-	public Locator skillName() {return skillName;}
-	
+
+	public Locator campaignName() {
+		return campaignName;
+	}
+
+	public Locator skills() {
+		return skills;
+	}
+
+	public Locator startDate() {
+		return startDate;
+	}
+
+	public Locator endDate() {
+		return endDate;
+	}
+
+	public Locator saveButton() {
+		return saveButton;
+	}
+
+	public Locator clickheretoAddskills() {
+		return clickheretoAddskills;
+	}
+
+	public Locator skillName() {
+		return skillName;
+	}
 
 }
